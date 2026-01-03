da68457fc88731c173c3e40ed9f26883734723a7
Danila <shumd100@gmail.com>
2026-01-03 20:30:18 +0400

    test api book

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -18,8 +18,15 @@ export const databaseProviders = [
 
       sequelize.addModels([Event, Booking]);
 
-      await sequelize.sync({ alter: true, force: false });
-      console.log('✅ Database synchronized successfully');
+      try {
+        await sequelize.authenticate();
+  
+          await sequelize.sync({ alter: true, force: false });
+          console.log('✅ Database synchronized successfully');
+        } catch (error) {
+        console.error('❌ Unable to connect to the database:', error);
+      }
+
       return sequelize;
     },
   },
